1f666423f8b62b3c42193cdb4e352861064f0421
Abdur Rahman Ayon <abdurrahmanayon66@gmail.com>
2025-09-25 15:58:35 +0600

    Implement Footer component with social media links and copyright information

--- frontend/src/components/generalComponents/Footer.tsx
+++ frontend/src/components/generalComponents/Footer.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
+import { RiFacebookFill, RiInstagramFill } from "react-icons/ri";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="bg-[#1E1E1E] px-[150px] py-[38px] flex justify-between items-center">
+      <div className="flex justify-center items-center">
+        <span className="text-white text-xl mr-[18px]">Follow us on</span>
+        <Link className="p-2.5 bg-white rounded-full mr-2" href="/">
+          <RiFacebookFill className="size-[24px]" />
+        </Link>
+        <Link className="p-2.5 bg-white rounded-full" href="/">
+          <RiInstagramFill className="size-[24px]" />
+        </Link>
+      </div>
+      <div className="text-xl text-white">Copyrights @ 2025 All rights reserved by <span className="text-[#67C18C]">beaded bangladesh</span></div>
+       <div className="text-xl text-white">Made with love by <span className="text-[#67C18C]">Abdur Rahman Ayon</span></div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
