cb8d6ddc83d1dc47d06d8c85f2b42384fa14f20b
Abdur Rahman Ayon <abdurrahmanayon66@gmail.com>
2025-09-25 15:48:23 +0600

    Refactor ProductCard component to enhance layout and add interactive buttons for improved user experience

--- frontend/src/components/product/ProductCard.tsx
+++ frontend/src/components/product/ProductCard.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Image from "next/image";
 import { TbCurrencyTaka } from "react-icons/tb";
+import { LuHeart, LuShoppingBag } from "react-icons/lu";
 
 interface ProductCardProps {
   image: string;
@@ -9,16 +10,29 @@ interface ProductCardProps {
   price: number;
 }
 
-const ProductCard: React.FC<ProductCardProps> = ({ image, category, name, price }) => {
+const ProductCard: React.FC<ProductCardProps> = ({
+  image,
+  category,
+  name,
+  price,
+}) => {
   return (
     <div className="max-w-[390px]">
-      <Image
-        src={image}
-        alt="product"
-        width={500}
-        height={500}
-        className="object-cover 2xl:w-[390px] 2xl:h-[495px]"
-      />
+      <div className="relative overflow-hidden group">
+        <Image
+          src={image}
+          alt="product"
+          width={500}
+          height={500}
+          className="object-cover 2xl:w-[390px] 2xl:h-[495px]"
+        />
+        <div className="bg-white/35 absolute w-full bottom-0 h-[100px] transform translate-y-[120%] transition-transform duration-300 ease-out group-hover:translate-y-0">
+          <div className="flex justify-center h-full items-center gap-x-3 text-[#7D7D7D]">
+            <button className="bg-white p-3 rounded-full"><LuHeart className="size-[24px]" /></button>
+            <button className="bg-white p-3 rounded-full"><LuShoppingBag className="size-[24px]" /></button>
+          </div>
+        </div>
+      </div>
       <div className="flex flex-col mt-[18px] uppercase items-center">
         <h1 className="text-sm text-[#6D6D6D]">{category}</h1>
         <h2 className="mt-[12px] text-xl">{name}</h2>
